7301b7216f2efeec23c2bcf02d2cd8fc07018fa4
DongMin Lee <skdddfhg@naver.com>
2019-12-27 18:10:27 +0900

    라우터 미드웨어 등록

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var app = express()
 var path = require('path')
 require('dotenv').config()
 
+var indexrouter = require('./router/index')
+var userrouter = require('./router/user')
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.set('html', require('ejs').renderFile)
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexrouter)
+app.use('/people', userrouter)
 
 var port = process.env.PORT || 3000
 app.listen(port, ()=>{
